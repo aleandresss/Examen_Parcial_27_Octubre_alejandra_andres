3eba9c6664a7718d763a3e83995825c6bf674883
aleandresss <114179514+aleandresss@users.noreply.github.com>
2022-10-27 19:38:47 +0200

    creo el str y añado los apellidos

---
+++ ejercicio4.py
@@ -0,0 +1,23 @@
+class alumno ():
+    def __init__ (self,nombre,nota,apellido)
+        self.nombre = nombre
+        self.nota = nota 
+        self.apellido
+
+    def __str__(self):
+        return "el alumno,{},se ha creado con éxito".format( self.nombre = nombre)
+
+    def aprobado(self):
+        if (self.nota > 4):
+            print ("el alumno ha aprobado")
+        else: 
+            print ("el alumno ha suspendido")
+        
+from alumno import*
+alumno1 = alumno("Alejandra","Andres",8)
+alumno2 = alumno("Pablo","Gonzales",5)
+alumno3 = alumno("Sofia","Grcia",3)
+
+    def __str__(self)
+        return alumno1.__str__+ ", con el nombre {} y apellido {},y {} con la nota ".format(self.nombre,self.apellido,self.nota)
+
